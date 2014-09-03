d99b29f134226e3a9110e4bb45db7a1377ef04a2
Wayner Valverde <waystanley14@hotmail.es>
2014-09-03 01:12:21 -0600

    Estructuras de datos(ListaDoble y Nodo)

---
+++ WD Player/src/wd/player/ListaDoble.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wd.player;
+import java.io.File;
+/**
+ *
+ * @author Way
+ */
+
+public class ListaDoble{
+	private Nodo first=null;
+	private Nodo last=null;
+	private int size;
+	
+	
+	ListaDoble(){// constructor
+		first=null;
+		last=null;
+		size=0;
+	}
+	
+	 public void add(String nombre,String artista, String album, String genero,int duracion,File ruta) 
+	{
+       	Nodo nodo = new Nodo(nombre,artista,album,genero,duracion,ruta);
+
+		// caso de lista vacía
+		if (this.size == 0) 
+		{ 
+			this.first = nodo;
+			this.last = nodo;
+			this.first.setPrevious(this.last);
+			this.first.setNext(this.last);
+			
+		}
+		else 
+		{	nodo.setPrevious(this.last);
+			nodo.setNext(this.last.getNext() );
+			this.last.setNext(nodo);
+			this.last = nodo;
+		}
+
+		this.size++;
+    }
+	
+	public Nodo buscarArtista(String artista){
+		if(this.first==null){
+			return null;
+		}
+		else{
+			/*Nodo actual=new Nodo();
+			actual=this.first;
+			for(int i=0; i<this.size;i++){
+				if(actual.getNombre==artista){
+					return actual;
+				}
+				else{
+					actual=actual.getNext();
+				}
+				
+			}*/
+			return null;
+		}
+		
+	}
+	
+
+	
+}
---
+++ WD Player/src/wd/player/Nodo.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wd.player;
+import java.io.File; 
+
+/**
+ *
+ * @author Way
+ */
+public class Nodo{
+
+		private String nombre;
+		private String artista;
+		private String album;
+		private String genero;
+		private int duracion;
+		//private loquesea imagen;
+		private File ruta;
+		private Nodo previous;
+		private Nodo next;
+		
+		Nodo (String nombre,String artista, String album, String genero,int duracion,File ruta){// Constructor Nodo
+			this.nombre=nombre;
+			this.artista=artista;
+			this.album=album;
+			this.genero=genero;
+			this.duracion=duracion;
+		// this.imagen;
+			this.ruta=ruta;
+			previous=null;
+			next=null;
+			
+		}
+		Nodo(){
+			this.nombre=" ";
+			this.artista=" ";
+			this.album=" ";
+			this.genero=" ";
+			this.duracion=0;
+		// this.imagen;
+			this.ruta=null;
+			previous=null;
+			next=null;
+		}
+		
+				
+		public String getNombre(){ // obtener el dato
+			return nombre;
+		}
+		public String getArtista(){ // obtener el dato
+			return artista;
+		}
+		public String getalbum(){ // obtener el dato
+			return album;
+		}
+		public String getGenero(){ // obtener el dato
+			return genero;
+		}
+		public int getDuracion(){ // obtener el dato
+			return duracion;
+		}
+		public File getRuta(){ // obtener el dato
+			return ruta;
+		}
+		public Nodo getNext() // Obtener siguiente
+		{
+            return next;
+			
+        }
+			public Nodo getPrevious()  // Obtener anterior
+		{
+            return previous;
+			
+        }
+		
+		public void setNombre(String nombre){ // obtener el dato
+			this.nombre=nombre;
+		}
+		public void setArtista(String artista){ // obtener el dato
+			this.artista=artista;
+		}
+		public void setAlbum(String album){ // obtener el dato
+			this.album=album;
+		}
+		public void setGenero(String genero){ // obtener el dato
+			this.genero=genero;
+		}
+		public void setDuracion(int duracion){ // obtener el dato
+			this.duracion=duracion;
+		}
+		
+		
+		public void setNext(Nodo n){ // Metodo asignar siguiente
+			this.next= n;
+			
+		}
+		
+		public void setPrevious(Nodo n){ // Metodo asignar anterior
+			this.previous= n;
+			
+		}
+		public void vaciar(){
+			this.nombre=" ";
+			this.artista=" ";
+			this.album=" ";
+			this.genero=" ";
+			this.duracion=0;
+		// this.imagen;
+			this.ruta=null;
+			previous=null;
+			next=null;
+		}
+		
+		
+	
+	}
